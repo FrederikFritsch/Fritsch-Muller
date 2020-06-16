e3365d56c840d4c79aa7a934d6187dff3b1365a1
FrederikFritsch <47738491+FrederikFritsch@users.noreply.github.com>
2020-06-16 19:48:03 +0200

    Added falling bats and random spawn.

---
+++ Space Shooter/Assets/Scripts/BatBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatBehaviour : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(this.transform.position.y <= -5.0f) {
+            Debug.Log("BordeFörstöra nåt nu!");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Space Shooter/Assets/Scripts/SpawnBats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBats : MonoBehaviour
+{
+
+    public GameObject batPrefab;
+    public Vector3 position;
+    void Start()
+    {
+        InvokeRepeating("spawnBat", 0, 5);
+    }
+
+    void spawnBat() {
+        position = new Vector3(Random.Range(-8.0f, 8.0f), 5.0f, 0.0f);
+        Instantiate(batPrefab, position, Quaternion.identity);
+    }
+}
